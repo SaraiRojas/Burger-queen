76e8a2a6dfe8870ac71529967309c3288b9cb76b
Maggie-Sarmiento <gsmaggie001@gmail.com>
2022-04-29 16:12:03 -0500

    Implementation of router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LogIn from './No-Authenticate/LogIn';
 import './App.css'; 
 
 const App = () => {
   return (
-    <LogIn />
+    <Router>
+      <Routes>
+        <Route path='/' element={<LogIn />}/>
+      </Routes>
+    </Router>
   );
 }
 
