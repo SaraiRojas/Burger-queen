b60495a65cffda6a918a15eddf6259aa1fb2f3bc
Sarai Rojas <sara.r.sandoval@gmail.com>
2022-05-03 10:10:01 -0500

    Gets user rol when the user logs in

--- src/Authenticate/Staff.jsx
+++ src/Authenticate/Staff.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../Firebase/firebase.config";
@@ -5,13 +6,14 @@ import { signOut } from "firebase/auth";
 import Sidebar from "../Sidebar";
 import styles from './Staff.module.css'
 
-const Staff = () => {
+const Staff = ({ rol }) => {
   const navigate = useNavigate();
 
   const handleLogOut = async () => {
     await signOut(auth);
     navigate('/');
   }
+  console.log(rol);
 
   return (
     <section className={styles.staff}>
--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBeAxZdrdC5Ckyudbb7qjX-AW4U8DSgEWQ',
@@ -13,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
