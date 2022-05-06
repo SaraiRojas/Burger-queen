0db0e74367a7b0bdf77aca0f95ea52c3dd6e2325
Maggie-Sarmiento <gsmaggie001@gmail.com>
2022-05-06 18:25:39 -0500

    Create the create and read on table admin

--- src/Authenticate/views/Staff.jsx
+++ src/Authenticate/views/Staff.jsx
@@ -16,13 +16,8 @@ import styles from './Staff.module.css';
 
 const Staff = () => {
   const [dataStaff, setDataStaff] = useState([]);
-  const [modalAdd, setModalAdd] = useState(false);
   const [modalEdit, setModalEdit] = useState(false);
   const [modalDelete, setModalDelete] = useState(false);
-
-  const openCLoseModalAdd = () => {
-    setModalAdd(!modalAdd);
-  };
 
   const openCLoseModalEdit = () => {
     setModalEdit(!modalEdit);
@@ -37,24 +32,6 @@ const Staff = () => {
       .then((response) => response.json())
       .then((data) => setDataStaff(data));
   }, []);
-
-  // console.log(dataStaff);
-
-  const bodyAdd = (
-    <div>
-      <h3>Agregar Empleado</h3>
-      <TextField name="nombre" label="Nombre" />
-      <br />
-      <TextField name="apellido" label="Apellido" />
-      <br />
-      <TextField name="rol" label="Rol" />
-      <br />
-      <div align="right">
-        <Button color="primary">Insertar</Button>
-        <Button>Cancelar</Button>
-      </div>
-    </div>
-  );
 
   const bodyEdit = (
     <div>
@@ -71,7 +48,7 @@ const Staff = () => {
       <br />
       <div align="right">
         <Button color="primary">Editar</Button>
-        <Button>Cancelar</Button>
+        <Button onClick={() => openCLoseModalEdit()}>Cancelar</Button>
       </div>
     </div>
   );
@@ -111,11 +88,11 @@ const Staff = () => {
               {dataStaff.map((empleado) => (
                 <TableRow key={empleado.id}>
                   <TableCell>{empleado.id}</TableCell>
-                  <TableCell>{empleado.nombre}</TableCell>
-                  <TableCell>{empleado.apellido}</TableCell>
-                  <TableCell>{empleado.rol}</TableCell>
-                  <TableCell>{empleado.correo}</TableCell>
-                  <TableCell>{empleado.fecha}</TableCell>
+                  <TableCell>{empleado.name}</TableCell>
+                  <TableCell>{empleado.lastname}</TableCell>
+                  <TableCell>{empleado.role}</TableCell>
+                  <TableCell>{empleado.email}</TableCell>
+                  <TableCell>{empleado.date}</TableCell>
                   <TableCell>
                     <EditIcon onClick={openCLoseModalEdit} />
                     &nbsp;&nbsp;&nbsp;
@@ -126,13 +103,6 @@ const Staff = () => {
             </TableBody>
           </Table>
         </TableContainer>
-
-        <Modal
-          open={modalAdd}
-          onClose={openCLoseModalAdd}
-        >
-          {bodyAdd}
-        </Modal>
 
         <Modal
           open={modalEdit}
