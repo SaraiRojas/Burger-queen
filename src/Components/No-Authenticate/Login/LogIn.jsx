15121121c430aadc18924a5a3c990a8cbf198b9a
Sarai Rojas <sara.r.sandoval@gmail.com>
2022-05-18 13:17:42 -0500

    Changes rute

--- src/Components/No-Authenticate/Login/LogIn.jsx
+++ src/Components/No-Authenticate/Login/LogIn.jsx
@@ -40,7 +40,7 @@ const LogIn = () => {
 
     if (expEmail.test(email) && expPassword.test(password)) {
       await signInWithEmailAndPassword(email, password);
-      navigate('/waiter');
+      navigate('/admin');
       // if (role === 'Admin') {
       //   navigate('/admin');
       // } else if (role === 'Mesero') {
